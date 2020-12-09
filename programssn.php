b1c34c00bc564b8f7a32c4a641b82d00e117bf45
AxayacatlS <axayacatls252@gmail.com>
2020-12-09 11:30:35 -0600

    version 6

---
+++ programssn.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Conecta tu aprendizaje | Cursos</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="js/jquery-1.5.2.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>
+<script type="text/javascript" src="js/Molengo_400.font.js"></script>
+<script type="text/javascript" src="js/Expletus_Sans_400.font.js"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<style type="text/css">.bg, .box2{behavior:url("js/PIE.htc");}</style>
+<![endif]-->
+</head>
+<body id="page3">
+<div class="body1">
+  <div class="main">
+    <!-- header -->
+    <header>
+      <div class="wrapper">
+        <nav>
+          <ul id="menu">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="programssn.php">Cursos</a></li>
+            <li><a href="login.php">Ingresar</a></li>
+            
+            <!--<li class="end"><a href="contacts.html">Contacts</a></li>-->
+          </ul>
+        </nav>
+        <ul id="icons">
+          <!--
+          <li><a href="#"><img src="images/icons1.jpg" alt=""></a></li>
+          <li><a href="#"><img src="images/icons2.jpg" alt=""></a></li>
+          -->
+        </ul>
+      </div>
+      <div class="wrapper">
+        <h1><a href="index.php"><div id="slogan">CONECTA TU APRENDIZAJE </div></a></h1>
+      </div>
+      <div id="slogan">La educacion debe estar <span>al alcance de todos</span> </div>
+    </header>
+    <!-- / header -->
+  </div>
+</div>
+<div class="body2">
+  <div class="main">
+    <!-- content -->
+    <section id="content">
+      <div class="box1">
+        <div class="wrapper">
+          <article class="col1">
+            <div class="pad_left1">
+              <h2 class="pad_bot1">Aprendamos</h2>
+            </div>
+            <div class="wrapper pad_bot2">
+              <!--AGREGAR IMAGEN-->
+              <!--<figure class="left marg_right1"><img src="images/page3_img1e.jpg" alt=""></figure>-->
+              <p class="pad_bot1 pad_top2"><strong>Versatilidad</strong> <br>
+                La plataforma tendra acceso a todo aquel que haga uso de ella</p>
+              <!--<a href="#" class="button marg_left1"><span><span>Read More</span></span></a> </div>-->
+            <div class="wrapper pad_bot2">
+              <!--AGREGAR IMAGEN-->
+              <!--<figure class="left marg_right1"><img src="images/page3_img2e.jpg" alt=""></figure>-->
+              <p class="pad_bot1 pad_top2"><strong>Auditiva</strong> <br>
+                Si no dispone de acceso puede escucharnos a traves de la radio</p>
+              <a href="index.php" class="button marg_left1"><span><span>Escuchar radio</span></span></a> </div>
+            <div class="wrapper">
+              <!--AGREGAR IMAGEN-->
+              <!--<figure class="left marg_right1"><img src="images/page3_img3e.jpg" alt=""></figure>-->
+              <p class="pad_bot1 pad_top2"><strong>Establecer un perfil</strong> <br>
+                Puede ingresar la sistema para llevar un mejor control de sus cursos</p>
+              <a href="registro.php" class="button marg_left1"><span><span>Registrarse</span></span></a> </div>
+          </article>
+          <article class="col2 pad_left2">
+            <div class="pad_left1">
+              <h2>Cursos</h2>
+            </div>
+            <ul class="list1">
+              <li><a href="#">Lectura</a></li>
+              <li><a href="#">Historia</a></li>
+              <li><a href="#">Matematicas</a></li>
+              <li><a href="#">Ciencias</a></li>
+              <li><a href="#">Etica</a></li>
+              <li><a href="#">Computacion</a></li>
+            </ul>
+            <!--
+            <div class="pad_left1">
+              <h2>Testimonials</h2>
+              <p class="quot"> <span><strong><a href="#">William Horner</a></strong></span> <span class="color1">Managing Director</span> <span class="color2 pad_bot1">Company Name</span> Nam libero tempore, cum soluta nobis esteligendi optio cumque nihil impedit quo minusid quod maxime placeat facere possimus, omnis voluptas assumenda estmnis dolor repellendus. </p>
+            </div>
+            <a href="#" class="button marg_top1"><span><span>&nbsp;&nbsp; View All &nbsp;&nbsp;</span></span></a> </article>
+            -->
+        </div>
+      </div>
+    </section>
+    <!-- content -->
+    <!-- footer -->
+    <footer>
+      <div class="wrapper">
+        <div class="pad1">
+          <div class="pad_left1">
+            <div class="wrapper">
+              <article class="call"> <span class="call1">Contactenos: </span><span class="call2">555-555-55-55</span> </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- / footer -->
+  </div>
+</div>
+<script type="text/javascript">Cufon.now();</script>
+</body>
+</html>
